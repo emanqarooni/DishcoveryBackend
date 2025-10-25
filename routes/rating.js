717e44b93c4777c44efa61868773177cbc2a92be
Fatima-Zaid <fatimaameer957@gmail.com>
2025-10-25 17:06:35 +0300

    add routes for recipe

---
+++ routes/rating.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+const recipeCtrl = require('../controllers/recipe')
+const upload = require('../middleware/upload')
+
+router.get('/',recipeCtrl.getRecipe)
+router.post('/createRecipe',upload.single('image'),recipeCtrl.createRecipe)
+router.put('/:recipeId',recipeCtrl.updateRecipe)
+router.delete('/:recipeId', recipeCtrl.deleteRecipe)
+
+module.exports = router
