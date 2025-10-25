79d684952b10f7c2a01a763fa50bda36d664cca4
Fatima-Zaid <fatimaameer957@gmail.com>
2025-10-25 17:20:09 +0300

    fix recipe routes

--- server.js
+++ server.js
@@ -22,9 +22,11 @@ app.use(express.static("public"))
 
 //require router
 const authRouter = require("./routes/auth")
+const recipeRouter = require("./routes/recipe")
 
 //use router
 app.use("/auth", authRouter)
+app.use("/recipe", recipeRouter)
 
 app.use("/", (req, res) => {
   res.send(`Connected!`)
