46a012a5de89c08eed28fd5a594c31d08347066a
Fatima-Zaid <fatimaameer957@gmail.com>
2025-10-26 14:12:42 +0300

    add toggle fav controller

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,14 +1,26 @@
-const router = require('express').Router()
-const recipeCtrl = require('../controllers/recipe')
-const upload = require('../middleware/upload')
+const router = require("express").Router()
+const recipeCtrl = require("../controllers/recipe")
+const upload = require("../middleware/upload")
+const middleware = require("../middleware")
 
-router.get('/',recipeCtrl.getRecipe)
-router.get('/:recipeId',recipeCtrl.getDetails)
-router.post('/createRecipe',upload.single('image'),recipeCtrl.createRecipe)
-router.put('/:recipeId',recipeCtrl.updateRecipe)
-router.delete('/:recipeId', recipeCtrl.deleteRecipe)
+router.get("/", recipeCtrl.getRecipe)
+router.get("/:recipeId", recipeCtrl.getDetails)
+router.post("/createRecipe", upload.single("image"), recipeCtrl.createRecipe)
+router.put("/:recipeId", recipeCtrl.updateRecipe)
+router.delete("/:recipeId", recipeCtrl.deleteRecipe)
 
-router.post('/:recipeId/addFav' ,recipeCtrl.favRecipe)
-router.delete('/:recipeId/deleteFav', recipeCtrl.favRecipeDelete)
+router.post(
+  "/:recipeId/toggleFav",
+  middleware.stripToken,
+  middleware.verifyToken,
+  recipeCtrl.toggleFavRecipe
+)
+
+router.get(
+  "/:recipeId/favStatus",
+  middleware.stripToken,
+  middleware.verifyToken,
+  recipeCtrl.checkFavStatus
+)
 
 module.exports = router
